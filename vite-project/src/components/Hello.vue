c53b514d0b6d456860fff1fcee04bcaed5bd7608
JacobCN <coder211@163.com>
2021-12-13 13:06:37 +0800

    Add simple vue3 component Hello.vue

---
+++ vite-project/src/components/Hello.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
+
+<template>
+  <h1 class="label">{{ msg }}</h1>
+</template>
+
+<style scoped>
+.label {
+  margin: 0 0.5em;
+  font-weight: bold;
+  color: darkcyan;
+}
+</style>
